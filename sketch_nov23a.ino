9a3a5dc4deacd4bd96e9715142131df18fc5d785
Sussic <31184868+Sussic@users.noreply.github.com>
2021-11-23 13:16:44 +0000

    Add files via upload
    arduino ultrasound test - works but needs calibration

---
+++ sketch_nov23a.ino
@@ -0,0 +1,27 @@
+#define trigPin 13
+#define echoPin 12
+
+void setup() {
+  Serial.begin (115200);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+  if (distance >= 600 || distance <= 0){
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  delay(500);
+}
