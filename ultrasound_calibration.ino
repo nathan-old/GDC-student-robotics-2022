8669ce34157ade7fb14844f40417ba80d8e74e20
Robertki16 <48026355+Robertki16@users.noreply.github.com>
2021-11-24 08:26:40 +0000

    Ultrasound calibration program
    The ultrasound will only take a reading when the button pin is pulled high, this can be used for calibration and a graph can be made on the serial plotter in Arduino IDE to find a value to convert the sensor to cm.

---
+++ ultrasound_calibration.ino
@@ -0,0 +1,40 @@
+#define trigPin 12
+#define echoPin 11
+#define buttonPin 10
+
+int buttonState = 0;
+
+void setup() {
+    Serial.begin (115200);
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+    pinMode(buttonPin, INPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void distance() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    long duration, distance;
+    digitalWrite(trigPin, LOW); 
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    distance = (duration/2);
+    Serial.println(distance);
+    Serial.print(" ");
+    // return distance;
+    delay(2000);
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
+void loop() {
+    buttonState = digitalRead(buttonPin);
+    if (buttonState == HIGH) {
+        // Run distance function
+        distance();
+    }
+}
+
+// use serial plotter to calculate values for distance measured then calculate the gradient as a convertion factor.
