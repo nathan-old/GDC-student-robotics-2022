afa2c889c8847b9c15190bdc6e3ad5a8a340dced
Leo Cornelius <leocornelius019@gmail.com>
2022-01-14 11:57:58 +0000

    Fix buzzerManager
    Need to call Super().init(...)

--- robot/buzzer.py
+++ robot/buzzer.py
@@ -1,27 +1,31 @@
+import threading
 import time
 from robot_module import RobotModule
 from sr.robot3 import Note
 
+
 class BuzzerManager(RobotModule):
-    def __init__(self, buzzer, min_time_between_buzzes=0.1):
+
+    def __init__(self, buzzer, min_time_between_buzzes=0.2):
+        super().__init__("buzzer", "Buzzer manager", "0.0.1", "0.0.0", "1.0.0")
         self.buzzer = buzzer
         self.min_time_between_buzzes = min_time_between_buzzes
-    
-    def playtone(self, tone, duration=0.13):
+
+    def _playtone(self, tone, duration=0.13):
+
+        if (duration * 1000) > 60000:
+            print("Playing tone: " + str(tone) + " for " + str(duration) +
+                  " which is over 65secconds (abort)")
+            return
         self.buzzer.buzz(duration, tone)
-    
-    def play_sequence(self, sequence): # sequence format: [ [tone, duration], [tone, duration], ... ]
-        for i in range(len(sequence)):
-            if (sequence[i][0] == 'pause'):
-                time.sleep(sequence[i][1])
+
+    def play_sequence(
+        self, sequence
+    ):  # sequence format: [ [tone, duration], [tone, duration], ... ]
+        for part in sequence:
+            if part[0] <= 20: # inauble so used as a "sleep"
+                time.sleep(part[1])
             else:
-                self.playtone(sequence[i][0], sequence[i][1])
+                self._playtone(part[0], part[1])
+                time.sleep(part[1])
             time.sleep(self.min_time_between_buzzes)
-    
-    def time_to_play(self, sequence):
-        time_to_play = 0
-        for i in range(len(sequence)):
-            time_to_play += sequence[i][1] + self.min_time_between_buzzes
-        # better to wait a bit longer than necessary
-        time_to_play += self.min_time_between_buzzes
-        return time_to_play
