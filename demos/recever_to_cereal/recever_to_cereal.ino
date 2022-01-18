fe1ebd00b0b9a614a25d8ee8ceb12c8fa1647a03
Robertki16 <48026355+Robertki16@users.noreply.github.com>
2022-01-18 13:23:23 +0000

    cereal box

---
+++ demos/recever_to_cereal/recever_to_cereal.ino
@@ -0,0 +1,178 @@
+
+
+#include <PinChangeInterrupt.h>
+#include <PinChangeInterruptBoards.h>
+#include <PinChangeInterruptPins.h>
+#include <PinChangeInterruptSettings.h>
+
+// First Example in a series of posts illustrating reading an RC Receiver with
+// micro controller interrupts.
+//
+// Subsequent posts will provide enhancements required for real world operation
+// in high speed applications with multiple inputs.
+//
+// http://rcarduino.blogspot.com/
+//
+// Posts in the series will be titled - How To Read an RC Receiver With A Microcontroller
+
+// See also http://rcarduino.blogspot.co.uk/2012/04/how-to-read-multiple-rc-channels-draft.html 
+
+
+#define NEUTRAL_POSITION 1500 
+
+#define SWITCH_SIGNAL_IN 16
+#define SWITCH_SIGNAL_IN_PIN 0
+volatile int nSwitchIn = NEUTRAL_POSITION;
+volatile unsigned long ulSStartPeriod = 0;
+volatile boolean bNewSwitchSignal = false;
+
+#define BIND_SIGNAL_IN 21
+#define BIND_SIGNAL_IN_PIN 5
+volatile int nBindIn = NEUTRAL_POSITION;
+volatile unsigned long ulBStartPeriod = 0;
+volatile boolean bNewBindSignal = false;
+
+#define THROTTLE_SIGNAL_IN 22
+#define THROTTLE_SIGNAL_IN_PIN 6
+volatile int nThrottleIn = NEUTRAL_POSITION;
+volatile unsigned long ulTStartPeriod = 0;
+volatile boolean bNewThrottleSignal = false;
+
+#define AILERON_SIGNAL_IN 18
+#define AILERON_SIGNAL_IN_PIN 2
+volatile int nAileronIn = NEUTRAL_POSITION;
+volatile unsigned long ulAStartPeriod = 0;
+volatile boolean bNewAileronSignal = false;
+
+#define ELEVATOR_SIGNAL_IN 19
+#define ELEVATOR_SIGNAL_IN_PIN 3
+volatile int nElevatorIn = NEUTRAL_POSITION; 
+volatile unsigned long ulEStartPeriod = 0; 
+volatile boolean bNewElevatorSignal = false;
+
+#define RUDDER_SIGNAL_IN 20
+#define RUDDER_SIGNAL_IN_PIN 4
+volatile int nRudderln = NEUTRAL_POSITION; 
+volatile unsigned long ulRStartPeriod = 0; 
+volatile boolean bNewRudderSignal = false;
+
+void setup() {
+  attachPinChangeInterrupt(ELEVATOR_SIGNAL_IN, calcElevator, HIGH);
+  attachPinChangeInterrupt(AILERON_SIGNAL_IN, calcAileron, HIGH);
+  attachPinChangeInterrupt(RUDDER_SIGNAL_IN, calcRudder, HIGH);
+  attachPinChangeInterrupt(THROTTLE_SIGNAL_IN, calcThrottle, HIGH);
+  attachPinChangeInterrupt(SWITCH_SIGNAL_IN, calcSwitch, HIGH);
+  attachPinChangeInterrupt(BIND_SIGNAL_IN, calcBind, HIGH);
+  Serial.begin(9600);
+}
+
+void loop() {
+ if(bNewAileronSignal) {
+   Serial.print(nAileronIn);
+   Serial.print(","); 
+   bNewAileronSignal = false;
+ }
+ if(bNewElevatorSignal) {
+   Serial.print(nElevatorIn);
+   Serial.print(","); 
+   bNewElevatorSignal = false;
+ }
+ if(bNewRudderSignal) {
+   Serial.print(nRudderln);
+   Serial.print(",");
+   bNewRudderSignal = false;
+ }
+ if(bNewSwitchSignal) {
+   Serial.print(nSwitchIn);
+   Serial.print(",");
+   bNewSwitchSignal = false;
+ }
+ if(bNewBindSignal) {
+   Serial.print(nBindIn);
+   Serial.print(",");
+   bNewBindSignal = false;
+ }
+  if(bNewThrottleSignal) {
+   Serial.println(nThrottleIn);
+   bNewThrottleSignal = false;
+ }
+ delay(100);
+}
+
+void calcBind() {
+  if(digitalRead(BIND_SIGNAL_IN_PIN) == HIGH) {
+    ulBStartPeriod = micros();
+  }
+  else {
+    if(ulBStartPeriod && (bNewBindSignal == false)) {
+      nBindIn = (int)(micros() - ulBStartPeriod);
+      ulBStartPeriod = 0;
+      bNewBindSignal = true;
+    }
+  }
+}
+
+void calcSwitch() {
+  if(digitalRead(SWITCH_SIGNAL_IN_PIN) == HIGH) {
+    ulSStartPeriod = micros();
+  }
+  else {
+    if(ulSStartPeriod && (bNewSwitchSignal == false)) {
+      nSwitchIn = (int)(micros() - ulSStartPeriod);
+      ulSStartPeriod = 0;
+      bNewSwitchSignal = true;
+    }
+  }
+}
+
+void calcElevator() {
+  if(digitalRead(ELEVATOR_SIGNAL_IN_PIN) == HIGH) {
+    ulEStartPeriod = micros();
+  }
+  else {
+    if(ulEStartPeriod && (bNewElevatorSignal == false)) {
+      nElevatorIn = (int)(micros() - ulEStartPeriod);
+      ulEStartPeriod = 0;
+      bNewElevatorSignal = true;
+    }
+  }
+}
+
+void calcAileron() {
+  if(digitalRead(AILERON_SIGNAL_IN_PIN) == HIGH) {
+    ulAStartPeriod = micros();
+  }
+  else {
+    if(ulAStartPeriod && (bNewAileronSignal == false)) {
+      nAileronIn = (int)(micros() - ulAStartPeriod);
+      ulAStartPeriod = 0;
+      bNewAileronSignal = true;
+    }
+  }
+}
+
+void calcRudder() {
+  if(digitalRead(RUDDER_SIGNAL_IN_PIN) == HIGH) {
+    ulRStartPeriod = micros();
+  }
+  else {
+    if(ulRStartPeriod && (bNewRudderSignal == false)) {
+      nRudderln = (int)(micros() - ulRStartPeriod);
+      ulRStartPeriod = 0;
+      bNewRudderSignal = true;
+    }
+  }
+}
+
+void calcThrottle() {
+  if(digitalRead(THROTTLE_SIGNAL_IN_PIN) == HIGH) {
+    ulTStartPeriod = micros();
+  }
+  else {
+    if(ulTStartPeriod && (bNewThrottleSignal == false)) {
+      nThrottleIn = (int)(micros() - ulTStartPeriod);
+      ulTStartPeriod = 0;
+      bNewThrottleSignal = true;
+    }
+  }
+}
---
+++ demos/robot.py
@@ -0,0 +1,90 @@
+import time, math, serial, numpy
+from sr.robot3 import *
+
+
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+    SER = hwid.split(' ')[2][4:]
+    print((port, hwid.split(' ')[2][4:]))
+    if SER == '7523031383335161C151':
+        serial_port = port
+        RUGGEDUINO_ID = SER
+ser = serial.Serial(serial_port)
+R = Robot(auto_start=True, ignored_ruggeduinos=[RUGGEDUINO_ID])
+
+ruggeduino_device = R.ignored_ruggeduinos[RUGGEDUINO_ID]
+ser = serial.Serial(ruggeduino_device, baudrate=9600)
+x_value = 0
+y_value = 0
+
+x_max = 1928
+x_min = 1088
+y_max = 1976
+y_min = 1088
+r_max = 1940
+r_min = 1104
+t_max = 1968
+t_min = 1096
+
+def range_map(value, max_, min_, ranges):
+    return round((ranges[0] + (ranges[1] - ranges[0]) * ((value-min_)/(max_-min_))),2)
+
+x = numpy.array([[-0.33, 0.58, 0.33], [-0.33, -0.58, 0.33], [0.67, 0, 0.33]])
+
+R.wait_start()
+
+while True:
+##    print(i)
+    arr = ser.readline().decode().rstrip().split(',')
+
+    x_value, y_value, r_value, s_value, b_value, t_value = int(arr[0]), int(arr[1]), int(arr[2]),int(arr[3]),int(arr[4]),int(arr[5])
+    if s_value < 1500:
+        s_state = True
+    else:
+        s_state = False
+    if b_value > 1500:
+        b_state = True
+    else:
+        b_state = False
+##    print(s_state, b_state, t_value)
+    x_value = range_map(x_value, x_max, x_min,[-1,1])
+    y_value = range_map(y_value, y_max, y_min,[-1,1])
+    r_value = range_map(r_value, r_max, r_min,[-1,1])
+    t_value = range_map(t_value, t_max, t_min,[0,1])
+    if s_state == True:
+        t_value *= -1
+
+    y = numpy.array([[x_value], [t_value], [r_value]])
+
+    A_speed = (round(numpy.dot(x,y)[0][0],1)) 
+    B_speed = (round(numpy.dot(x,y)[1][0],1)) 
+    C_speed = (round(numpy.dot(x,y)[2][0],1))
+    
+    if A_speed > 1:
+        A_Speed = 1
+    elif A_speed < -1:
+        A_speed = -1
+    if B_speed > 1:
+        B_Speed = 1
+    elif B_speed < -1:
+        B_speed = -1
+    if C_speed > 1:
+        C_Speed = 1
+    elif C_speed < -1:
+        C_speed = -1
+    
+    R.motor_boards["SR0PJ1W"].motors[0].power = A_speed
+    R.motor_boards["SR0PJ1W"].motors[1].power = B_speed
+    R.motor_boards["SR0VG1M"].motors[0].power = C_speed
+##    print(A_speed, B_speed, C_speed)
+ser.close()
+
+
+
+
+
+'''
+http://thetechnicgear.com/2014/04/howto-build-3-wheels-holonomic-robot-using-lego
+Arduino ID needs to be found and entered - Don't know how it didn't work last time I tried.
+'''
