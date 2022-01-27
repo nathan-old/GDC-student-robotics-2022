25ea589492a860670a852e30196b7860bb623775
Robertki16 <robertkimber123@gmail.com>
2022-01-27 10:25:51 +0000

    robot position finding and plotting codes

---
+++ demos/computer position calculator.py
@@ -0,0 +1,67 @@
+from math import *
+import matplotlib.pyplot as plt
+
+# calculates the distance of the marker from the robot
+def calculate_distance(theta,psi,r, num):
+	h = (r*cos(radians(90-theta)))
+	z = (r*cos(radians(theta)))
+	x = (h*cos(radians(90-psi)))
+	y = (h*sin(radians(90-psi)))
+	print("x:",x,end = ', ')
+	print("y:",y,end = ', ')
+	print("z:",z)
+	return calculate_pos(x, y, marker_list, num)
+
+# calculates the position of the robot using the position of the marker
+def calculate_pos(x_in,y_in,marker_list,marker_number):
+	robot_pos = []
+	for i in range(len(marker_list)):
+			if marker_list[i][0] == marker_number:
+				marker_pos = [marker_list[i][1],marker_list[i][2]]
+	if marker_number >= 0 and marker_number <= 6:
+		x = marker_pos[0]-y_in
+		y = marker_pos[1]-x_in
+		robot_pos = [x,y]
+	elif marker_number >= 7 and marker_number <= 13:
+		x = marker_pos[0]-x_in
+		y = marker_pos[1]-y_in
+		robot_pos = [x,y]
+	elif marker_number >= 14 and marker_number <= 20:
+		x = marker_pos[0]+y_in
+		y = marker_pos[1]+x_in
+		robot_pos = [x,y]
+	elif marker_number >= 19 and marker_number <= 27:
+		x = marker_pos[0]+x_in
+		y = marker_pos[1]+y_in
+		robot_pos = [x,y]
+	return(robot_pos)
+
+# list of marker positions
+marker_list = [[0,718.75,5750],[1,1437.5,5750],[2,2156.25,5750],[3,2875,5750],[4,3593.75,5750],[5,4312.5,5750],[6,5031.25,5750],[7,5750,5031.25],[8,5750,4312.5],[9,5750,3593.75],[10,5750,2875],[11,5750,2156.25],[12,5750,1437.5],[13,5750,718.75],[14,5031.25,0],[15,4312.5,0],[16,3593.75,0],[17,2875,0],[18,2156.25,0],[19,1437.5,0],[20,718.75,0],[21,0,718.75],[22,0,1437.5],[23,0,2156.25],[24,0,2875],[25,0,3593.75],[26,0,4312.5],[27,0,5031.25]]
+
+# inputs from robot camera in degrees and mm
+# list of markers seen by camera, in order theta, psi, r, marker number
+markers = [[85,45,150,11],[90,90,200,8],[90,90,550,17], [90,90,5750/2,3],[90,90,5750/2,17]]
+
+
+
+marker_list = [[0,718.75,5750],[1,1437.5,5750],[2,2156.25,5750],[3,2875,5750],[4,3593.75,5750],[5,4312.5,5750],[6,5031.25,5750],[7,5750,5031.25],[8,5750,4312.5],[9,5750,3593.75],[10,5750,2875],[11,5750,2156.25],[12,5750,1437.5],[13,5750,718.75],[14,5031.25,0],[15,4312.5,0],[16,3593.75,0],[17,2875,0],[18,2156.25,0],[19,1437.5,0],[20,718.75,0],[21,0,718.75],[22,0,1437.5],[23,0,2156.25],[24,0,2875],[25,0,3593.75],[26,0,4312.5],[27,0,5031.25]]
+x = []
+y = []
+for i in range(len(marker_list)):
+	x.append(marker_list[i][1])
+	y.append(marker_list[i][2])
+plt.scatter(x,y, linewidths = 5)
+
+for i in range(len(x)):
+	#plt.annotate('hi', 5000, 500)
+	plt.text(x[i], y[i], str(i), horizontalalignment='center', verticalalignment='center', fontsize = 6)
+
+
+for i in markers:
+	point = calculate_distance(i[0],i[1],i[2], i[3])
+	plt.scatter(point[0],point[1])
+
+#plt.show()
+
+plt.savefig(fname = 'figure.png', dpi = 200)
