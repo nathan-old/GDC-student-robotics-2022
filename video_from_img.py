6c1701343ddd4e739ea0f98cf42b8e431c3fb038
Robertki16 <robertkimber123@gmail.com>
2022-02-06 16:39:13 +0000

    MP4 and % completion

--- video_from_img.py
+++ video_from_img.py
@@ -8,8 +8,10 @@ for filename in glob.glob('*.jpg'):
     size = (width,height)
     img_array.append(img)
 
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 2, size)
- 
+#out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 2, size)
+out = cv2.VideoWriter('output.mp4', cv2.VideoWriter_fourcc(*'MP4V'), 10, size)
+
 for i in range(len(img_array)):
+    print(str(round((i/len(img_array)*100),2))+'%')
     out.write(img_array[i])
 out.release()
