433198785d7e380881c3e7d9de1cd079a6e321aa
Robertki16 <robertkimber123@gmail.com>
2022-02-16 15:54:17 +0000

    Working, accurate position tracking.

--- demos/position tracking.py
+++ demos/position tracking.py
@@ -4,22 +4,21 @@ long single beep = turn
 double beep = got new position
 quadruple beep = got all positions
 '''
-from math import *
 from sr.robot3 import *
-import os
-import time
-rpm_data = [[0,-0.1,-0.2,-0.3,-0.4,-0.5,-0.6,-0.7,-0.8,-0.9,-1.0],[0,7.042253521,18.01801802,30.92783505,42.55319149,50.84745763,65.2173913,85.71428571,88.23529412,89.82035928,90.09009009]]
+import os, time, math
 
 R = Robot(auto_start=True, verbose=True)
 wheels = [R.motor_boards['SR0PJ1W'].motors[0],R.motor_boards['SR0PJ1W'].motors[1],R.motor_boards['SR0VG1M'].motors[0]] #array of motor commands
 arm_radius = 0.28 # radius of the arms of the robot/ m
-circumfrence = 0.3299
-motor_power = 0.1
-#rpm = rpm_data[1][1]
-rpm = 122.03*(motor_power)
+circumfrence = 0.3299 # wheel circumference/ m
+motor_power = 0.2
+rpm = 4.6778*(motor_power**6) - 88.46*(motor_power**5) - 9.1788*(motor_power**4) + 82.827*(motor_power**3) + 5.6922*(motor_power**2) + 97.405*(motor_power)
 speed = circumfrence * (rpm/60) # speed of the motors at power = 0.5/ m/s
-print(rpm)
+print('Running with an rpm of ' + str(rpm) + 'rpm and a power of '+str(motor_power))
 data_filename = os.path.join(R.usbkey, 'data')
+
+reverse_what_distance = 200
+turn_what_angle = 10
 
 def save_data(data, mode):
 	output = open(data_filename+'.txt',mode)
@@ -27,18 +26,23 @@ def save_data(data, mode):
 	if mode != 'w':
 		output.write(data+'\n')
 	output.close()
+
 def rotate(angle):
-	distance = angle * ((pi * 2 * arm_radius) / 360)
+	negative = 1
+	if angle < 0:
+		angle *= -1
+		negative = -1
+	distance = angle * ((math.pi * 2 * arm_radius) / 360)
 	print('Turning ' + str(angle) + ' degrees')
 	time_ = float(distance / speed)
 	for wheel in wheels:
-		wheel.power = motor_power
+		wheel.power = motor_power*negative
 	time.sleep(time_)
 	for wheel in wheels:
 		wheel.power = BRAKE
 
 def reverse(x_distance):
-	distance = x_distance/cos(radians(30))
+	distance = (x_distance/1000)/math.cos(math.radians(30))
 	print('Moving '+str(x_distance)+' meters backwards')
 	time_ = float(distance / speed)
 	wheels[0].power = motor_power
@@ -48,7 +52,7 @@ def reverse(x_distance):
 	wheels[1].power = BRAKE
 
 def get_intersections(x0, y0, r0, x1, y1, r1):
-	d=sqrt((x1-x0)**2 + (y1-y0)**2)
+	d=math.sqrt((x1-x0)**2 + (y1-y0)**2)
 	if d > r0 + r1 :
 		return [[-1,-1],[-1,-1]]
 	if d < abs(r0-r1):
@@ -57,7 +61,7 @@ def get_intersections(x0, y0, r0, x1, y1, r1):
 		return [[-1,-1],[-1,-1]]
 	else:
 		a=(r0**2-r1**2+d**2)/(2*d)
-		h=sqrt(r0**2-a**2)
+		h=math.sqrt(r0**2-a**2)
 		x2=x0+a*(x1-x0)/d   
 		y2=y0+a*(y1-y0)/d   
 		x3=x2+h*(y1-y0)/d     
@@ -67,7 +71,7 @@ def get_intersections(x0, y0, r0, x1, y1, r1):
 		return ([[x3, y3], [x4, y4]])
 
 def calculate_distance(theta,r):
-	horizontal_distance = (r*cos(radians(theta)))
+	horizontal_distance = (r*math.cos(math.radians(theta)))
 	return horizontal_distance
 
 def get_markers():
@@ -75,17 +79,16 @@ def get_markers():
 	inputs from robot camera in degrees and mm
 	list of markers seen by camera, in order theta, psi, r, marker number
 	'''
-	turn_what_angle = 10
 	rotation = 0
 	retry_count = 0
 	markers = []
-	while len(markers) != 2:
+	while len(markers) < 2:
 		markers = []
-		time.sleep(0.5)
 		markers_input = R.camera.see()
 		for marker in markers_input:
-			markers.append([degrees(marker.spherical.rot_x),degrees(marker.spherical.rot_y),marker.spherical.dist,marker.id])
-		if len(markers) != 2:
+			markers.append([math.degrees(marker.spherical.rot_x),math.degrees(marker.spherical.rot_y),marker.spherical.dist,marker.id])
+		print('seen: ' + str(len(markers)) + ' markers')
+		if len(markers) < 2 or len(markers) > 3:
 			retry_count += 1
 			if retry_count < 3:
 				R.power_board.piezo.buzz(0.5, 400)
@@ -98,7 +101,7 @@ def get_markers():
 				if rotation == 360:
 					rotation = 0
 					print('reached full turn, moving back')
-					reverse(0.2)
+					reverse(reverse_what_distance)
 	return markers
 save_data('','w')
 # list of marker positions
@@ -117,39 +120,34 @@ for i in range(5):
 		#print(circles)
 		valid_points = [[],[]]
 		for i in range(len(circles)):
-				if i +1 != len(circles):
-						a= i+1
+			if len(circles) == 3:
+				if i + 1 != len(circles):
+					a = i+1
 				else:
-						a = 0
-				points = get_intersections(circles[i][0],circles[i][1],circles[i][2], circles[a][0],circles[a][1],circles[a][2])
-				for i in points:
-						if 0<i[0]<5750 and 0<i[1]<5750:
-								valid_points[0].append(i[0])
-								valid_points[1].append(i[1])
+					a = 0
+			elif len(circles) == 2:
+				if i == len(circles):
+					pass
+				a = i+1
+			points = get_intersections(circles[i][0],circles[i][1],circles[i][2], circles[a][0],circles[a][1],circles[a][2])
+			for i in points:
+				if 0<i[0]<5750 and 0<i[1]<5750:
+					valid_points[0].append(i[0])
+					valid_points[1].append(i[1])
 		if len(valid_points[0]) > 1:
 			avg_x = sum(valid_points[0]) / len(valid_points[0])
 			avg_y = sum(valid_points[1]) / len(valid_points[1])
 			print('-------------------------------------------------------------------------')
 			print(seen_markers)
-			print('Found position - coordinate('+str(round(avg_x,3))+','+str(round(avg_y,3))+')-using average of 2\nContinueing in 5s')
-			print('-------------------------------------------------------------------------')
-			#save_data(str([avg_x,avg_y]),'a')
-		elif len(valid_points[0]) == 1:
-			avg_x = valid_points[0]
-			avg_y = valid_points[1]
-			print('-------------------------------------------------------------------------')
-			print(seen_markers)
-			print('Found position - coordinate('+str(round(avg_x,3))+','+str(round(avg_y,3))+')-using one point\nContinueing in 5s')
+			print('Found position - coordinate('+str(round(avg_x,3))+','+str(round(avg_y,3))+')-using average of ' + str(len(valid_points[0])) + ' points\nContinueing')
 			print('-------------------------------------------------------------------------')
 		else:
 			print('no valid intersections - this should not run')
-		R.power_board.piezo.buzz(0.2, 400)
-		time.sleep(0.2)
-		R.power_board.piezo.buzz(0.2, 400)
-		time.sleep(4.4)
+
 end_time = time.time()
 print('Data file saved to: ' + data_filename + '.txt')
 print('finished in '+str(end_time-start_time)+' Seconds')
+
 R.power_board.piezo.buzz(0.2, 400)
 time.sleep(0.2)
 R.power_board.piezo.buzz(0.2, 400)
